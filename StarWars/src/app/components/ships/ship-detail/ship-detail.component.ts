5a6ae093f17910e00c88eb5656aa0bad9a10c314
pfesenmeier@gmail.com <pfesenmeier@gmail.com>
2019-11-11 16:36:02 -0500

    component ts

--- StarWars/src/app/components/ships/ship-detail/ship-detail.component.ts
+++ StarWars/src/app/components/ships/ship-detail/ship-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ship-detail',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShipDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _activatedRoute: ActivatedRoute, private pokemon: ) { }
 
   ngOnInit() {
+    this._activatedRoute.paramMap.subscribe(routeData => {
+      console.log(routeData);
+    })
   }
 
 }
