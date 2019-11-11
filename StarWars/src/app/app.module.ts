85808773a47ba24aa2c2c11399710dc13e7aebac
pfesenmeier@gmail.com <pfesenmeier@gmail.com>
2019-11-11 14:51:03 -0500

    scaffold component

--- StarWars/src/app/app.module.ts
+++ StarWars/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShipDetailComponent } from './components/ships/ship-detail/ship-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShipDetailComponent
   ],
   imports: [
     BrowserModule
