3bf8930a090fe0c90bac2756fccff02478be1883
Aaron Floyd <aaronrfloyd@gmail.com>
2019-11-12 13:46:32 -0500

    NamesList

--- StarWars/src/app/components/ships/ship-index/ship-index.component.ts
+++ StarWars/src/app/components/ships/ship-index/ship-index.component.ts
@@ -16,7 +16,7 @@ export class ShipIndexComponent implements OnInit {
   private searchForm: FormGroup;
     
   columnNames = ['Name','Details']
-
+ 
   dataSource: MatTableDataSource<Pokemon>;
 
   constructor(private form: FormBuilder, private _shipService: ShipService, private router: Router) {
@@ -24,9 +24,9 @@ export class ShipIndexComponent implements OnInit {
    }
 
   ngOnInit() {
-      this._shipService.getShips().subscribe((pokemons: Pokemon[]) => {
-        this.dataSource = new MatTableDataSource<Pokemon>(pokemons);
-        console.log(pokemons);
+      this._shipService.getShips().subscribe((pokemons: any) => {
+        this.dataSource = new MatTableDataSource<Pokemon>(pokemons.results);
+        
       });
   }
 
